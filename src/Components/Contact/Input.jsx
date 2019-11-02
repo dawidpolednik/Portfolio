cacf8b5364d95a8111321c439e9e4602b4fa053c
Dawid <dawid.polednik@gmail.com>
2019-11-02 11:48:56 +0100

    change css

--- src/Components/Contact/Input.jsx
+++ src/Components/Contact/Input.jsx
@@ -3,18 +3,22 @@ import style from "./Contact.module.scss";
 
 const Input = ({ name, placeholder, value, onChange }) => {
   return (
-    <div className={`${style.col}`}>
-      <input
-        name={name}
-        className={style.effect}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-      <span className={style.focusBorder}>
-        <i></i>
-      </span>
-    </div>
+    <>
+      <label className={style.label} for={name}></label>
+      <div className={`${style.col}`}>
+        <input
+          name={name}
+          id={name}
+          className={style.effect}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        <span className={style.focusBorder}>
+          <i></i>
+        </span>
+      </div>
+    </>
   );
 };
 
--- src/Components/Contact/TextArea.jsx
+++ src/Components/Contact/TextArea.jsx
@@ -3,21 +3,25 @@ import style from "./Contact.module.scss";
 
 const TextArea = ({ name, placeholder, value, onChange }) => {
   return (
-    <div className={`${style.col}`}>
-      <textarea
-        name={name}
-        className={style.effect}
-        rows="15"
-        cols="50"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        style={{ resize: "none" }}
-      ></textarea>
-      <span className={style.focusBorder}>
-        <i></i>
-      </span>
-    </div>
+    <>
+      <label className={style.label} for={name}></label>
+      <div className={`${style.col}`}>
+        <textarea
+          name={name}
+          className={style.effect}
+          rows="15"
+          cols="50"
+          placeholder={placeholder}
+          value={value}
+          id={name}
+          onChange={onChange}
+          style={{ resize: "none" }}
+        ></textarea>
+        <span className={style.focusBorder}>
+          <i></i>
+        </span>
+      </div>
+    </>
   );
 };
 
