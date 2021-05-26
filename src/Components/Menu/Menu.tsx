86dd3d058c76c7f53e0babfa8c22181eea8fbd90
Dawid Polednik <dawid.polednik@pirios.corp>
2021-05-26 08:55:59 +0200

    disable scroll when menu opened

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,24 +1,43 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import styles from './Menu.module.scss';
 import { CloseIcon } from './CloseIcon';
 import { useEscape } from './useEscape';
 import ReactDOM from 'react-dom';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+
 interface MenuProps {
   onClose: () => void;
   isOpen: boolean;
 }
 
 export const Menu: FC<MenuProps> = ({ isOpen, onClose }) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+
   useEscape(() => onClose());
+
+  useEffect(() => {
+    if (isOpen) disableBodyScroll(menuRef.current);
+  }, [isOpen]);
+
+  const handleOnClose = () => {
+    onClose();
+    enableBodyScroll(menuRef.current);
+  };
+
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
     <section
+      ref={menuRef}
       className={styles.wrapper}
-      onKeyDown={e => e.key === 'Escape' && onClose()}
+      onKeyDown={e => e.key === 'Escape' && handleOnClose()}
     >
       <div className={styles.contentWrapper}>
-        <div className={styles.closeIconWrapper} onClick={onClose}>
+        <div className={styles.closeIconWrapper} onClick={handleOnClose}>
           <CloseIcon />
         </div>
       </div>
